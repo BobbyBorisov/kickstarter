3f09b971e2ccc126109fec04321640887c251c0f
Bobby Borisov <borislav.borisov.tues@gmail.com>
2018-07-04 15:57:19 +0300

    add view request button and dummy controller

--- routes.js
+++ routes.js
@@ -2,3 +2,4 @@ const routes = (module.exports = require('next-routes')());
 
 routes.add('/campaigns/new', '/campaigns/new')
 routes.add('/campaigns/:address', '/campaigns/show');
+routes.add('/campaigns/:address/requests', '/campaigns/requests/index');
